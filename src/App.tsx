0b30de15122dc0506b1e9ac829fbfdb449f5404d
Thaise Dantas <thaiserpdantas@gmail.com>
2020-08-05 09:46:42 -0300

    :lipstick: Add responsive style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import './assets/styles/global.css'
+
 function App() {
   return (
     <div className="App">
       <h1>Hello!</h1>
+      <button type="button">clique aqui</button>
     </div>
   );
 }
